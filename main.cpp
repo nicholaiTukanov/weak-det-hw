5968f99b6ab8ae7d2a542ef7c67e3872cde48e42
nicholaiTukanov <nicholaitukanov@gmail.com>
2021-03-25 19:30:17 -0500

    Add comment describing logic of driver

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include "multicore.hpp"
 
-int main() {
-	std::cout << "Hello World!" << endl;
+
+/*
+
+	This is the driver for our custom simulator. This driver will take in an instruction stream, 
+	and pass it through our simulator. After the instruction stream has finished, our driver
+	will output logfiles similar to Pin.
+
+	This driver is currently a work in progress.
+
+*/
+
+
+
+int main(int argc, char *argv[]) {
+
+
+	std::cout << "Custom hardware simulator for experimenting with weak determinism models." << endl;
 	return 0;
+
+
 }
